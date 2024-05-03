bbf433ec5d6c1a17b690c20eea270025e1662ca4
Leah Somerville <leah.somerville4@gmail.com>
2024-05-03 10:09:09 -0500

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Leah Somerville on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
